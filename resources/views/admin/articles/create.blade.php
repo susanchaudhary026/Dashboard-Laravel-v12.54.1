984970f932e5c3f7aa9d96a4136733b66168722e
Susan Chaudhary <susanchaudharyyy@outlook.com>
2026-05-11 10:35:52 +0545

    Add: Exception Catcher if any error occurs

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 <div class="card p-4 shadow-sm">
+        @if ($errors->any())
+    <div class="alert alert-danger">
+        @foreach ($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    </div>
+        @endif
     <div class="d-flex justify-content-between align-items-center mb-4">
         <h2>Create New Article</h2>
         <a href="{{ route('articles.index') }}" class="btn btn-outline-secondary btn-sm">Back to List</a>
